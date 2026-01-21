c3d38f6b8076c01469963a778aac9207ad50b34b
Osama Farag <ofarag115@gmail.com>
2026-01-21 11:10:04 -0500

    fixed CSRF Verification Failed Requested Aborted issue.

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,11 +28,22 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ALLOWED_HOSTS = [
+#    'localhost',
+#    'https://ofarag115-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+#]
+
+# CSRF_TRUSTED_ORIGINS = [
+#    'https://ofarag115-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/'
+#    ]
 ALLOWED_HOSTS = [
-    'localhost',
-    'https://ofarag115-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    "localhost",
+    ".proxy.cognitiveclass.ai",
 ]
-CSRF_TRUSTED_ORIGINS = ['https://ofarag115-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ofarag115-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -54,6 +65,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
