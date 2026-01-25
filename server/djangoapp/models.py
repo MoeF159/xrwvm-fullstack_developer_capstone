96489a3d25547daa16eafcb100591eb884cc93fb
Osama Farag <129105851+MoeF159@users.noreply.github.com>
2026-01-25 14:20:46 -0500

    Update Linter for models.py
    testing Linter

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -23,11 +23,15 @@ class CarMake(models.Model):
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
+
 # Car Models, using ForeignKey field)
+
 # - Name
 # - Type (CharField with a choices argument to provide limited choices
+
 # such as Sedan, SUV, WAGON, etc.)
 # - Year (IntegerField) with min value 2015 and max value 2023
+
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
@@ -59,7 +63,6 @@ class CarModel(models.Model):
     )
 
     # Other fields as needed
-
 
     def __str__(self):
         return f"{self.car_make.name} {self.name} ({self.year})"
